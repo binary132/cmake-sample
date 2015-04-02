ab4065bd5a309aafad1b16abf07d03cb7c54a757
Bodie Solomon <binary132@gmail.com>
2015-04-01 22:31:13 -0400

    Put working simple project together

---
+++ include/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+namespace hello {
+  int printHello() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+  }
+}
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "hello.cpp"
+
+int main(int argc, char** argv) {
+  return hello::printHello();
+}
